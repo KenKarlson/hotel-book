ee2a03c99798f2748f6539f836e919bf3fe0bbe7
Ken Karlson <javarecords@mail.ru>
2022-01-11 02:59:00 +0300

    add api php form

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,9 @@
 'use strict';
-//import Typed from '../js/typed.js';
 let typed = new Typed('.header__title', {
   strings: ["Забронируйте номер прямо сейчас", "Проведите лучший отпуск в нашем отеле"],
   typeSpeed: 120,
   loop: true,
-  loopCount: Infinity,
+  loopCount: 3,
   showCursor: false
 });
+
