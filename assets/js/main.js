0f55846c5ee8016ec6f6d866d08ac977cd067539
Ken Karlson <javarecords@mail.ru>
2022-01-10 22:56:44 +0300

    Create: test php files

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,6 @@ let typed = new Typed('.header__title', {
   strings: ["Забронируйте номер прямо сейчас", "Проведите лучший отпуск в нашем отеле"],
   typeSpeed: 120,
   loop: true,
-  loopCount: Infinity,
+  loopCount: 3,
   showCursor: false
 });
